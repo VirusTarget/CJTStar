9fc99da1fc2209f11d98e8e833066935e087101e
陈晋添 <408069830@qq.com>
2017-03-26 13:30:22 +0800

    调整代码规范

--- Star/CJTStar/CJTStarView2.m
+++ Star/CJTStar/CJTStarView2.m
@@ -6,104 +6,111 @@
 //  Copyright © 2016年 CJT. All rights reserved.
 //
 
-#import "StarView2.h"
+#import "CJTStarView2.h"
 
-@implementation StarView2
+@interface CJTStarView2(){
+@private NSInteger starCount;//星星个数
+@private CGFloat width;//每个星星的平均宽度
+}
+@property (nonatomic, strong) UIView *frontView;//前置视图
+@end
 
--(instancetype)initWithFrame:(CGRect)frame AndStarCount:(NSInteger)StarCount
+@implementation CJTStarView2
+
+-(instancetype)initWithFrame:(CGRect)frame starCount:(NSInteger)StarCount
 {
     if (self = [super initWithFrame:frame]) {
         
         /*初始化*/
         [self setUserInteractionEnabled:YES];
-        star_count = StarCount;
+        starCount = StarCount;
         CGRect rect = self.frame;
         width = rect.size.width/StarCount;
         self.backgroundColor = [UIColor whiteColor];
         
         for (int count = 0; count < StarCount; count++) {
             //设置初始的图像
-            UIImageView *StarImage = [[UIImageView alloc] initWithImage:self.EmptyImage];
+            UIImageView *StarImage = [[UIImageView alloc] initWithImage:self.emptyImage];
             StarImage.frame = CGRectMake(count*width, 0, width, rect.size.height);
             [StarImage setContentMode:UIViewContentModeScaleToFill];
             [self addSubview:StarImage];
             
         }
         
-        //设置前置图片
-        FrontView = [[UIView alloc] initWithFrame:self.bounds];
-        //设置子视图范围不超过父视图
-        FrontView.clipsToBounds = YES;
         for (int count = 0; count < StarCount; count++) {
             //设置初始的图像
-            UIImageView *StarImage = [[UIImageView alloc] initWithImage:self.FullImage];
+            UIImageView *StarImage = [[UIImageView alloc] initWithImage:self.fullImage];
             StarImage.frame = CGRectMake(count*width, 0, width, rect.size.height);
             [StarImage setContentMode:UIViewContentModeScaleToFill];
-            [FrontView addSubview:StarImage];
+            [self.frontView addSubview:StarImage];
         }
-        NSLog(@"%f",FrontView.frame.size.width);
-        [self addSubview:FrontView];
+        NSLog(@"%f",self.frontView.frame.size.width);
+        [self addSubview:self.frontView];
     }
     return self;
 }
 
-#pragma mark 触摸响应
+#pragma mark- event response
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
     [event touchesForView:self];
     NSSet *allTouches = [event allTouches];    //返回与当前接收者有关的所有的触摸对象
     UITouch *touch = [allTouches anyObject];   //视图中的所有对象
     CGPoint point = [touch locationInView:[touch view]]; //返回触摸点在视图中的当前坐标
-    int x = point.x;
     
-    _NowScore = x/width;
-    _NowScore = _NowScore<star_count?_NowScore:star_count;
-    _NowScore = _NowScore>0?_NowScore:0;
-    
-    //获得百分比
-    double precent = _NowScore/star_count;
-    CGRect rect = self.bounds;
-    rect.size.width = precent*rect.size.width;
-    //设置新宽度
     [UIView animateWithDuration:0.1 animations:^{
-        FrontView.frame = rect;
+        [self starImagesCalcByX:point.x];
     }];
-    
 }
 -(void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
     NSSet *allTouches = [event allTouches];    //返回与当前接收者有关的所有的触摸对象
     UITouch *touch = [allTouches anyObject];   //视图中的所有对象
     CGPoint point = [touch locationInView:[touch view]]; //返回触摸点在视图中的当前坐标
-    int x = point.x;
     
-    _NowScore = x/width;
-    _NowScore = _NowScore<star_count?_NowScore:star_count;
-    _NowScore = _NowScore>0?_NowScore:0;
+    [self starImagesCalcByX:point.x];
     
-    double precent = _NowScore/star_count;
+}
+
+#pragma mark- private method
+/// 计算当点击在 X 上时，每个位置上的星星图片
+- (void)starImagesCalcByX:(int)x {
+    _nowScore = x/width;
+    _nowScore = _nowScore<starCount?_nowScore:starCount;
+    _nowScore = _nowScore>0?_nowScore:0;
+    
+    //获得百分比
+    double precent = _nowScore/starCount;
     CGRect rect = self.bounds;
-    rect.size.width = precent*self.bounds.size.width;
-    FrontView.frame = rect;
-    
+    rect.size.width = precent*rect.size.width;
+    //设置新宽度
+        self.frontView.frame = rect;
 }
-
-#pragma mark 懒加载
--(UIImage *)EmptyImage
+#pragma mark- getter/setter
+-(UIImage *)emptyImage
 {
-    if (!_EmptyImage) {
-        _EmptyImage = [UIImage imageNamed:@"star-empty"];
+    if (!_emptyImage) {
+        _emptyImage = [UIImage imageNamed:@"star-empty"];
     }
-    return _EmptyImage;
+    return _emptyImage;
 }
 
 
--(UIImage *)FullImage
+-(UIImage *)fullImage
 {
-    if (!_FullImage) {
-        _FullImage = [UIImage imageNamed:@"star--full"];
+    if (!_fullImage) {
+        _fullImage = [UIImage imageNamed:@"star--full"];
     }
-    return _FullImage;
+    return _fullImage;
 }
 
+- (UIView *)frontView {
+    if (!_frontView) {
+        //设置前置图片
+        _frontView = [[UIView alloc] initWithFrame:self.bounds];
+        //设置子视图范围不超过父视图
+        _frontView.clipsToBounds = YES;
+    }
+    return _frontView;
+}
 @end
--- Star/ViewController.m
+++ Star/ViewController.m
@@ -7,8 +7,8 @@
 //
 
 #import "ViewController.h"
-#import "StarView.h"
-#import "StarView2.h"
+#import "CJTStarView.h"
+#import "CJTStarView2.h"
 
 @interface ViewController ()
 
@@ -20,15 +20,12 @@
     [super viewDidLoad];
     CGSize size = [[UIScreen mainScreen] bounds].size;
     
-    StarView *Star = [[StarView alloc] initWithFrame:CGRectMake(100, 100, size.width-200, 50) AndStarCount:5];
-    Star.OpenHalf = YES;
-    [self.view addSubview:Star];
-    [self.view setUserInteractionEnabled:YES];
+    CJTStarView *star = [[CJTStarView alloc] initWithFrame:CGRectMake(100, 100, size.width-200, 50) starCount:5];
+    star.openHalf = NO;
+    [self.view addSubview:star];
     
-    StarView2 *Star2 = [[StarView2 alloc] initWithFrame:CGRectMake(100, 300, size.width-200, 50) AndStarCount:5];
-    [self.view addSubview:Star2];
-    [self.view setUserInteractionEnabled:YES];
-    // Do any additional setup after loading the view, typically from a nib.
+    CJTStarView2 *star2 = [[CJTStarView2 alloc] initWithFrame:CGRectMake(100, 300, size.width-200, 50) starCount:10];
+    [self.view addSubview:star2];
 }
 
 - (void)didReceiveMemoryWarning {
